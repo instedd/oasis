40c6fea5dc5bab2916fac5dd4657d8fe60f0aed2
Ana Perez Ghiglia <aghiglia@manas.tech>
2020-05-19 17:26:48 -0300

    dockerize: add mysql basic contaier
    for #13

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
+import os
+import mysql.connector
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
+cnx = mysql.connector.connect(host=os.environ['DATABASE_HOST'], user='root', password='', database='covid')
+cursor = cnx.cursor()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 @app.route('/')
 async def homepage(request):
@@ -18,3 +21,12 @@ async def homepage(request):
 def read_root():
     return "Welcome to oasis api 🌴"
 
+@app.get('/api/getcounty')
+def getcounty():
+    cursor.execute("SELECT * from countycal where date between '2020-05-06' and '2020-05-06'")
+    row_headers=[x[0] for x in cursor.description] #this will extract row headers
+    rv = cursor.fetchall()
+    json_data=[]
+    for result in rv:
+         json_data.append(dict(zip(row_headers,result)))
+    return json_data
