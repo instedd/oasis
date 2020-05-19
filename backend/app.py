e65bfb6c8605b560b1588507cfc00099bb76bb26
Jules Morelli <jmorelli@manas.tech>
2020-05-19 16:50:56 -0300

    Add structure to serve UI from the python server

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
-@app.get("/")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.route('/')
+async def homepage(request):
+    template = "index.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context)
+
+@app.get("/api")
 def read_root():
-    return "Welcome to oasis api"
+    return "Welcome to oasis api 🌴"
+
