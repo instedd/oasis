84a90d47163c6259e22c4f7b8775e0fb842879be
Jules Morelli <jmorelli@manas.tech>
2020-05-21 11:30:57 -0300

    Remove unnecessary dependencies & reorganize db helper methods

--- backend/router/users.py
+++ backend/router/users.py
@@ -4,22 +4,16 @@ from fastapi import Depends, FastAPI, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
 from users import crud, models, schemas
-from database.database import SessionLocal
+from database.database import SessionLocal, get_db
 
 router = APIRouter()
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 @router.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
--- backend/users/crud.py
+++ backend/users/crud.py
@@ -11,6 +11,10 @@ def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
 
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
