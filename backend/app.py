bc8387d13133fe297c5d310bdcf6c8471076a944
Matías García Isaía <mgarcia@manas.tech>
2020-05-28 09:03:18 -0300

    Production Dockerfile + TravisCI builds (#37)
    * Add production Dockerfile
    We build a single image that serves the Python API + a
    static build of the frontend
    * Publish docker images on TravisCI
    * Make TravisCI use docker-compose
    * Tweak UI routing for served files

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
@@ -5,15 +7,23 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from router import users
 
-app = FastAPI()
+async def homepage(request, exec):
+    template = "index.html"
+    context = {"request": request}
+    return templates.TemplateResponse(template, context)
+
+app = FastAPI(exception_handlers={
+    404: homepage
+})
+
 templates = Jinja2Templates(directory='templates')
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 origins = [
-    'http://ui.oasis.lvh.me:3000',
-    'http://localhost:3000'
-]
+        'http://ui.oasis.lvh.me:3000',
+        'http://localhost:3000'
+    ] if os.environ.get('DEV') else []
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,27 +33,7 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-@app.route('/')
-async def homepage(request):
-    template = "index.html"
-    context = {"request": request}
-    return templates.TemplateResponse(template, context)
-
-@app.get("/api")
-def read_root():
-    return "Welcome to oasis api 🌴"
-
-# @app.get('/api/getcounty')
-# def getcounty():
-#     cursor.execute("SELECT * from countycal where date between '2020-05-06' and '2020-05-06'")
-#     row_headers=[x[0] for x in cursor.description] #this will extract row headers
-#     rv = cursor.fetchall()
-#     json_data=[]
-#     for result in rv:
-#          json_data.append(dict(zip(row_headers,result)))
-#     return json_data
-
-
 app.include_router(users.router, 
     prefix="/api",
-    tags=["users"])
+    tags=["users"]
+)
