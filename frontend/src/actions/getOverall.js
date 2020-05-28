d49e1dc200eee35f7147594dadc72399d6c002e9
Jules Morelli <jmorelli@manas.tech>
2020-05-28 15:28:03 -0300

    Change packages

--- frontend/src/actions/getOverall.js
+++ frontend/src/actions/getOverall.js
@@ -1,19 +1,17 @@
-import axios from "axios";
-import { GET_OVERALL } from "./types";
-// axios.defaults.withCredentials = true
+import { GET_OVERALL } from './types';
 
 export function getOverall() {
-  return function (dispatch) {
-    axios
-      .get("https://covid19api.herokuapp.com/latest")
-      .then((res) => {
-        dispatch({
-          type: GET_OVERALL,
-          data: res.data,
-        });
+  return function(dispatch){
+    console.log("hi")
+    fetch('https://covid19api.herokuapp.com/latest')
+    .then((res) => {
+      dispatch({
+        type: GET_OVERALL,
+        data:res.data
       })
       .catch((e) => {
         console.log(e);
       });
-  };
+    });
+  }
 }
