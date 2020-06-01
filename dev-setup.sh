231e18d096166e15459e0c53875bcd346894851a
Jules Morelli <jmorelli@manas.tech>
2020-06-01 11:42:01 -0300

    Improve dev-setup

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # set up precommit hooks (this actually needs to happen outside of docker!)
-yarn add pre-commit
+yarn
 /Applications/Python\ 2.7/Install\ Certificates.command
 pre-commit install -f
 
