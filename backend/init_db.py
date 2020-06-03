20100af6f38abb6ea3645cdab94ee8e8c0f47599
Jules Morelli <jmorelli@manas.tech>
2020-06-03 13:16:33 -0300

    Try something different

---
+++ backend/init_db.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+import os
+
+databases_to_create = ["dbtest", os.environ["DATABASE_NAME"]]
+
+engine = sqlalchemy.create_engine(
+    f"mysql+pymysql://{os.environ['DATABASE_USER']}"
+    f":{os.environ['DATABASE_PASSWORD']}"
+    f"@{os.environ['DATABASE_HOST']}"
+)
+# Query for existing databases
+existing_databases = engine.execute("SHOW DATABASES;")
+# Results are a list of single item tuples, so unpack each tuple
+existing_databases = [d[0] for d in existing_databases]
+
+for database in databases_to_create:
+    if database not in existing_databases:
+        engine.execute(f"CREATE DATABASE IF NOT EXISTS {database}")
+        print(f"Created database {database} 😎")
+    else:
+        print(f"Database {database} already exists 🥳")
