8f98b6245426843b1a4882953b4ef71e5e7ca298
Ana Perez Ghiglia <aghiglia@manas.tech>
2020-06-18 13:02:51 -0300

    change sex initial value in CriticalQuestions `null` is allowed in the backend, "" is not

--- frontend/src/routes/CriticalQuestions/index.js
+++ frontend/src/routes/CriticalQuestions/index.js
@@ -54,7 +54,7 @@ function CriticalQuestions(props) {
 
   const [formValues, setFormValues] = useState({
     age: null,
-    sex: "",
+    sex: null,
     ethnicity: "",
     currentLocation: "",
     postalCode: null,
