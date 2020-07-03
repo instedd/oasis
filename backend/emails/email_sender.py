42f37a5ce72495d5fb0565c9b2c531e680395fde
Ana Perez Ghiglia <aghiglia@manas.tech>
2020-07-03 18:42:48 -0300

    remove unnecessary code

--- backend/emails/email_sender.py
+++ backend/emails/email_sender.py
@@ -3,17 +3,11 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-
-def get_env(key):
-    return os.environ[key] if not IS_DEV else ""
-
-
-IS_DEV = os.environ.get("DEV", False)
-SENDER = get_env("SMTP_FROM_ADDRESS")
-USERNAME_SMTP = get_env("SMTP_USER")
-PASSWORD_SMTP = get_env("SMTP_PASS")
-HOST = get_env("SMTP_SERVER")
-PORT = get_env("SMTP_PORT")
+SENDER = os.environ.get("SMTP_FROM_ADDRESS", "")
+USERNAME_SMTP = os.environ.get("SMTP_USER", "")
+PASSWORD_SMTP = os.environ.get("SMTP_PASS", "")
+HOST = os.environ.get("SMTP_SERVER", "")
+PORT = os.environ.get("SMTP_PORT", "")
 
 
 def send(to: str, subject: str, body_text: str, body_html: str = None):
@@ -36,7 +30,9 @@ def send(to: str, subject: str, body_text: str, body_html: str = None):
 
     # Try to send the message.
     try:
-        if not IS_DEV:
+        if os.environ.get("DEV", False):
+            print(msg.as_string())
+        else:
             server = smtplib.SMTP(HOST, PORT)
             server.ehlo()
             server.starttls()
@@ -45,8 +41,6 @@ def send(to: str, subject: str, body_text: str, body_html: str = None):
             server.login(USERNAME_SMTP, PASSWORD_SMTP)
             server.sendmail(SENDER, to, msg.as_string())
             server.close()
-        else:
-            print(msg.as_string())
     # Display an error message if something goes wrong.
     except Exception as e:
         print("Error: ", e)
