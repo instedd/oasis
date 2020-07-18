345009744191e632fa7060c859fa47872f1247b5
tanaykarve <tanaykarve@gmail.com>
2020-07-18 16:03:08 -0400

    resolved alembic conflict

--- backend/alembic/versions/2551e1a3cb06_add_spam_column_to_stories.py
+++ backend/alembic/versions/2551e1a3cb06_add_spam_column_to_stories.py
@@ -20,7 +20,21 @@ def upgrade():
     op.add_column(
         "stories", sa.Column("spam", sa.Integer(), server_default="0")
     )
+    op.add_column(
+        "stories",
+        sa.Column(
+            "latitude", sa.Numeric(precision=10, scale=7), nullable=True
+        ),
+    )
+    op.add_column(
+        "stories",
+        sa.Column(
+            "longitude", sa.Numeric(precision=10, scale=7), nullable=True
+        ),
+    )
 
 
 def downgrade():
     op.drop_column("stories", "spam")
+    op.drop_column("stories", "longitude")
+    op.drop_column("stories", "latitude")
