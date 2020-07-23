1f4fea3a56cb07091f982733ae2bee1ee4545462
yus252 <yus252@ucsd.edu>
2020-07-22 22:13:29 -0700

    Delete unnecessary code

--- frontend/src/components/Map/index.js
+++ frontend/src/components/Map/index.js
@@ -29,6 +29,8 @@ export default function Map(props, { draggable = true }) {
   const fillOutlineColor = "rgba(86, 101, 115, 0.5)";
 
   const userStory = props.userStory;
+  console.log("User Story");
+  console.log(userStory);
 
   const dataScope = {
     WORLD: "world",
@@ -130,6 +132,8 @@ export default function Map(props, { draggable = true }) {
     const body = await api(`stories/all`, {
       method: "GET",
     });
+    console.log("All the stories");
+    console.log(body);
     return storiesToGeoJson(body);
   };
 
@@ -380,26 +384,11 @@ export default function Map(props, { draggable = true }) {
 
       // create the marker
       const sickStatus = marker.properties.sick;
-      if (sickStatus === sicknessStatus.SICK) {
-        new mapboxgl.Marker({ color: statusMapping[sickStatus].color })
-          .setLngLat(marker.geometry.coordinates)
-          .setPopup(popup) // sets a popup on this marker
-          .addTo(map);
-      }
-
-      if (sickStatus === sicknessStatus.NOT_SICK) {
-        new mapboxgl.Marker({ color: statusMapping[sickStatus].color })
-          .setLngLat(marker.geometry.coordinates)
-          .setPopup(popup)
-          .addTo(map);
-      }
-
-      if (sickStatus === sicknessStatus.RECOVERED) {
-        new mapboxgl.Marker({ color: statusMapping[sickStatus].color })
-          .setLngLat(marker.geometry.coordinates)
-          .setPopup(popup)
-          .addTo(map);
-      }
+      console.log(sickStatus);
+      new mapboxgl.Marker({ color: statusMapping[sickStatus].color })
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(popup) // sets a popup on this marker
+        .addTo(map);
     });
 
     // Add current user's marker
