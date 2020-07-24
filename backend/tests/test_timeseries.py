19e9177d969489079662e0f61e7cf0a194b4979a
Yingxi Lin <yxlin118@gmail.com>
2020-07-24 11:40:01 -0700

    remove time series test

--- backend/tests/test_timeseries.py
+++
@@ -1,9 +0,0 @@
-from time_series import crud, models
-
-
-def test_insert_current_data(setup):
-    """
-    Tests if the current time series file can be loaded from github
-    """
-    crud.init_table(setup["db"])
-    assert len(setup["db"].query(models.TimeSeries).all()) == 14
