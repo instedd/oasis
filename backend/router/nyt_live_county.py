eebfbbe6d826aa3888812fa591a010a998776b14
Alex Wenzel <atwenzel@eng.ucsd.edu>
2020-08-11 14:34:09 -0700

    check for empty query and stale data

--- backend/router/nyt_live_county.py
+++ backend/router/nyt_live_county.py
@@ -9,6 +9,7 @@ from database import get_db
 from NytLiveCounty import crud, schemas
 
 # from datetime import date
+import time
 
 router = APIRouter()
 
@@ -18,7 +19,18 @@ def get_county_data(county_ids: str, db: Session = Depends(get_db)):
     """
     Returns a list of county data records based on IDs (FIPS) queried
     """
+    # Get county ids, check for empty list
+    if county_ids == " ":
+        return JSONResponse([], status_code=200)
     county_ids = county_ids.split(",")
+
+    # Get result and encode
     res = crud.get_nyt_data(db, county_ids)
     res = jsonable_encoder(res)
+
+    # Check if database needs to be updated
+    if max([rec["timestamp"] for rec in res]) < time.time() - 60 * 60 * 6:
+        crud.update(db)
+
+    # Return result
     return JSONResponse(res, status_code=200)
