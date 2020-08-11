7b758fa078cae81b70669e7677f94e9a11ee5fe1
Alex Wenzel <atwenzel@eng.ucsd.edu>
2020-08-11 14:35:21 -0700

    additional unit tests for NYT county

--- backend/tests/test_nyt_county.py
+++ backend/tests/test_nyt_county.py
@@ -1,19 +1,35 @@
 import json
-from NytLiveCounty import crud, models
-
-
-def test_insert_current_data(setup):
-    """
-    Tests if the current NYT file can be loaded from github
-    """
-    crud.update(setup["db"])
-    assert len(setup["db"].query(models.NytLiveCounty).all()) > 0
-    assert setup["db"].query(models.NytLiveCounty).first().fips == "01001"
+from NytLiveCounty import models
+import time
+from sqlalchemy import func
 
 
 def test_query_counties(setup):
     """
     Tests if the two counties Cook and San Diego can be queried correctly
     """
+    # crud.update(setup["db"])
     response = setup["app"].get("/api/nyt_live_county/17031,06073")
-    assert len(json.loads(response.content)) == 2
+    assert len(json.loads(response.content)) == 28
+
+
+def test_empty_query(setup):
+    """
+    Tests that the result for an empty query is empty
+    """
+    # crud.update(setup["db"])
+    response = setup["app"].get("/api/nyt_live_county/ ")
+    print(type(json.loads(response.content)))
+    assert len(json.loads(response.content)) == 0
+
+
+def test_data_window(setup):
+    # crud.seed(setup["db"])
+
+    THIRTEEN_DAYS_AGO = time.time() - (60 * 60 * 24 * 13)
+    FIFTEEN_DAYS_AGO = time.time() - (60 * 60 * 24 * 15)
+
+    min_ts = setup["db"].query(func.min(models.NytLiveCounty.timestamp)).one()
+
+    assert min_ts[0] < THIRTEEN_DAYS_AGO
+    assert min_ts[0] > FIFTEEN_DAYS_AGO
