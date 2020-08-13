2afe4de2bce0f24abeb4714efc3f016ddf474ed3
yus252 <yus252@ucsd.edu>
2020-08-13 14:05:00 -0700

    change mystory prompt and add city to zipcode

--- frontend/src/routes/MyStory/index.js
+++ frontend/src/routes/MyStory/index.js
@@ -40,10 +40,9 @@ export default function MyStory(props) {
   return (
     <>
       <h1 className="title">MY COVID-19 STORY</h1>
-      <p className="subtitle">Your COVID-19 story in your own words</p>
       <TextField
         id="outlined-multiline-static"
-        placeholder="We want to learn from your experience to help overcome the pandemic. Even if you aren’t sick, we all have a COVID story. Share Your COVID-19 Story*"
+        placeholder="We want to learn from your experience to help overcome the pandemic. We all have a COVID-19 story, share yours!"
         multiline
         rowsMax={10}
         value={myStory}
