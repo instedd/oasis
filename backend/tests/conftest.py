0bfa9abcfdb50f61cdec9464e7e10597c9112a70
Alex Wenzel <atwenzel@eng.ucsd.edu>
2020-08-31 12:18:30 -0700

    reverting test config to current master version

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,22 +1,18 @@
 import pytest
 
 from sqlalchemy.orm import Session
-
-# import alembic.config
+import alembic.config
 from fastapi.testclient import TestClient
 
 from database import Base, engine
 from app import app
 
-# Uncomment this to test NYT county data
-# from NytLiveCounty.crud import seed
-
 
 @pytest.fixture(scope="module")
 def setup():
     client = TestClient(app)
+    alembic.config.main(argv=["upgrade", "head"])
     session = Session(bind=engine)
-    # seed(session)  # Uncomment me to test NYT county data
     yield {"app": client, "db": session}
     for tbl in reversed(Base.metadata.sorted_tables):
         engine.execute(tbl.delete())
