2e6ca3bf8708bc8e14c02126157a2f97175df5a5
Yingxi Lin <yxlin118@gmail.com>
2020-09-29 13:28:11 -0700

    revert

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # set up precommit hooks (this actually needs to happen outside of docker!)
-pip install pre-commit
-/Applications/Python\ 2.7/Install\ Certificates.command
-pre-commit install -f
+#pip install pre-commit
+#/Applications/Python\ 2.7/Install\ Certificates.command
+#pre-commit install -f
 
 # lift & update containers
-docker-compose build
-docker-compose run --rm api pip install -r requirements.txt
-docker-compose run --rm api pip install -r requirements.dev.txt
-docker-compose run --rm api python init_db.py
-docker-compose run --rm ui yarn
-docker-compose run --rm api alembic upgrade head
+docker-compose.exe build
+docker-compose.exe run --rm api pip install -r requirements.txt
+docker-compose.exe run --rm api pip install -r requirements.dev.txt
+docker-compose.exe run --rm api python init_db.py
+docker-compose.exe run --rm ui yarn
+docker-compose.exe run --rm api alembic upgrade head
