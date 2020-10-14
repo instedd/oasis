32bb93a7386d0407473d0c4fed6d331e524e8feb
Alex Wenzel <atwenzel@eng.ucsd.edu>
2020-10-13 21:24:01 -0700

    remove db param from seed

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,8 @@ from auth.main import NotFoundException
 import sqlalchemy
 from NytLiveCounty import crud
 
+# import asyncio
+
 
 async def homepage(request, exec):
     template = "index.html"
@@ -55,9 +57,16 @@ engine = sqlalchemy.create_engine(SQLALCHEMY_DATABASE_URL)
 Session = sqlalchemy.orm.sessionmaker(
     autocommit=False, autoflush=False, bind=engine
 )
+
+# @app.on_event("startup")
+# async def seed_db():
+#    asyncio.ensure_future(crud.seed())
+
+# asyncio.ensure_future(crud.seed())
+
 db = Session()
 
-crud.seed(db)
+crud.seed()
 
 # db.close()
 
